3ef26d01c966f8d2c3b74df942794e0f9c4a397f
William DeMeo <williamdemeo@gmail.com>
2016-03-13 00:23:42 -0600

    a bit of progress on Chapter 5

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,7 +3,9 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = 
+  	// The arrow `=>` in front of the argument type `B` means that the function `f` 
+  	// takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
@@ -17,16 +19,34 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = (this, n) match {
+  	case (Empty, _) => Empty
+  	case (_, 0) => this
+    case (Cons(h, t), m) => cons(h(), t().take(m-1))
+	}
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = (this, n) match {
+  	case (Empty, _) => Empty
+  	case (_, 0) => this
+  	case (Cons(h, t), m) => t().drop(m-1) 
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+  	case Empty => Empty
+  	case Cons(h,t) => if(p(h())) cons(h(), t() takeWhile(p))
+  										else t() takeWhile(p)	
+  }
 
   def forAll(p: A => Boolean): Boolean = sys.error("todo")
 
   def headOption: Option[A] = sys.error("todo")
 
+  def toList: List[A] = this match {
+  	case Empty => Nil
+  	case Cons(h, t) => h() :: t().toList
+  }
+  
+  
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
