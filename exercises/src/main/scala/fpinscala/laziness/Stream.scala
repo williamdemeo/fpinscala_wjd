5c578241d4119274867cfec9cd9c94894ffd593c
williamdemeo <williamdemeo@gmail.com>
2016-03-14 20:41:34 -0500

    added worksheet for Ch 5

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -13,75 +13,93 @@ trait Stream[+A] {
 	// Ex 5.1: Write a function to convert a Stream to a List, which will 
 	//         force its evaluation and let you look at it in the REPL. 
 	//         You can convert to the regular List type in the standard library. 
-	def toList: List[A] = this match {
+	def toList_first_try: List[A] = this match {
   	case Empty => Nil
-  	case Cons(h, t) => h() :: t().toList
+  	case Cons(h, t) => h() :: t().toList_first_try
   }
   
+	// As mentioned in the book, the first try above will stack overflow for large 
+	// streams.  Here is a tail-recursive version.
+	def toList: List[A] = {
+	  @annotation.tailrec
+	  def toList_tailrec(s: Stream[A], acc: List[A]): List[A] = s match {
+	    case Empty => acc
+	    case Cons(h, t) => toList_tailrec(t(), h()::acc)
+	  }
+	  toList_tailrec(this, Nil).reverse
+	}
+	// We could avoid the need for reverse by using a mutable list buffer. (See solutions.)
+	
   // Ex 5.2: Write the function take(n) for returning the first n elements of  
   //         a Stream, and drop(n) for skipping the first n elements of a Stream.
   // 5.2a
   def take(n: Int): Stream[A] = (this, n) match {
-  	case (Empty, _) => Empty
-  	case (_, 0) => Empty
-    case (Cons(h, t), m) => cons(h(), t().take(m-1))
+    case (Cons(h, t), m) if m > 1 => cons(h(), t().take(m-1))
+  	case (Cons(h, _), 1) => cons(h(), Empty)
+  	case _ => Empty
 	}
-  // 5.2b
-  def drop(n: Int): Stream[A] = (this, n) match {
-  	case (Empty, _) => Empty
-  	case (_, 0) => this
-  	case (Cons(h, t), m) => t().drop(m-1) 
+  
+  // 5.2b: (If we declare a public function tailrec, we must mark it `final`
+  //       so it can't be overwritten.)
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+  	case Cons(h, t) if n > 0 => t().drop(n-1) 
+  	case _ => this
   }
 
   // Ex 5.3: Write the function takeWhile for returning all *starting* elements 
   //         of a Stream that match the given predicate.
   def takeWhile(p: A => Boolean): Stream[A] = this match {
-  	case Empty => Empty
-  	case Cons(h,t) => if(p(h())) cons( h(), t() takeWhile(p) ) else Empty
+  	case Cons(h,t) if(p(h())) => cons( h(), t() takeWhile(p) )
+  	case _ => Empty
   }
 
   // My first implementation of takeWhile was wrong because I misread the spec.
   // It would be more accurately described as `takeIf`, defined as follows:
   def takeIf(p: A => Boolean): Stream[A] = this match {
-  	case Empty => Empty
-  	case Cons(h,t) => 
-  		if(p (h()) ) cons(h(), t() takeIf(p)) else t() takeIf(p)	
+  	case Cons(h,t) => if(p (h()) ) cons(h(), t() takeIf(p)) else t() takeIf(p)
+  	case _ => Empty
   }
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = 
-  	// The arrow `=>` in front of the argument type `B` means that the function `f` 
-  	// takes its second argument by name and may choose not to evaluate it.
-    this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) 
-                        // If `f` doesn't evaluate its second argument, 
-                        // the recursion never occurs.
-      case _ => z
-    }
-
+  // exists with explicit recursion (p. 70) 
+  def exists_first_try(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exists_first_try(p)
+    case _ => false
+  }
+  
+  // exists with general recursion (using foldRight)
   def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b) 
     // Here `b` is the unevaluated recursive step that folds the tail of the stream. 
     // If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
-  @annotation.tailrec
-  final def find(f: A => Boolean): Option[A] = this match {
-    case Empty => None
-    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
-  }
-
+  
 	// Ex 5.4: Implement forAll, which checks that all elements in the Stream 
   //         match a given predicate. Your implementation should terminate the 
   //         traversal as soon as it encounters a non-matching value.
+  // forAll with explicit recursion (first try)
   def forAll_first_try(p: A => Boolean): Boolean = this match {
   	case Empty => true
   	case Cons(h, t) => if (p(h())) t() forAll(p) else false
   }
-  def forAll(p: A => Boolean): Boolean = this match {
+  // forAll with explicit recursion (second try)
+  def forAll_second_try(p: A => Boolean): Boolean = this match {
   	case Empty => true
     case Cons(h, t) => p(h()) && t().forAll(p)
   }
-
+  // forAll with general recursion (using foldRight)
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a,b) => p(a) && b)
+    
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match { 
+  	// The arrow `=>` in front of the argument type `B` means that the function `f` 
+  	// takes its second argument by name and may choose not to evaluate it.
+    case Cons(h,t) => f(h(), t().foldRight(z)(f)) 
+    // If `f` doesn't evaluate its second argument, the recursion never occurs.
+    case _ => z
+  }
+    
   // Ex 5.5: Use foldRight to implement takeWhile.
-  def forAll_with_fold(p: A => Boolean): Boolean = sys.error("todo")
+  def takeWhile_with_fold(p: A => Boolean): Stream[A] = 
+    foldRight[Stream[A]](Empty)( (a,b) => {if(p(a)) cons(a, b) else Empty} )
 
   // Ex 5.6: (Hard) Implement headOption using foldRight.
   def headOption: Option[A] = sys.error("todo")
@@ -92,6 +110,12 @@ trait Stream[+A] {
   //         (Part of the exercise is writing your own function signatures.)
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  @annotation.tailrec
+  final def find(f: A => Boolean): Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
--- exercises/src/test/scala/Ch05_laziness.sc
+++ exercises/src/test/scala/Ch05_laziness.sc
@@ -2,44 +2,50 @@ package fpinscala.laziness
 import Stream._
 
 object Chapter05_scratch {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  println("Welcome to the Scala worksheet")
+  
   
   // Let's make sure we're using our stream library
   // (not the standard one):
-  Stream(1,2,3)                                   //> res0: fpinscala.laziness.Stream[Int] = Cons(<function0>,<function0>)
+  Stream(1,2,3)
 
+	// some streams we'll use for testing:
+	val abc = Stream(1,2,3)
+	val ones: Stream[Int] = Stream.cons(1, ones)
+  ones.take(5).toList
+  
   // toList() tests
   //   NB: toList forces each element of the stream to be evaluated
-  Stream(1,2,3).toList                            //> res1: List[Int] = List(1, 2, 3)
+  Stream(1,2,3).toList
 
   // take() tests
-  Stream(1,2,3).take(2).toList                    //> res2: List[Int] = List(1, 2)
-  Stream(1,2,3).take(0).toList                    //> res3: List[Int] = List()
-  Stream(1,2,3).take(4).toList                    //> res4: List[Int] = List(1, 2, 3)
+  Stream(1,2,3).take(2).toList
+  Stream(1,2,3).take(0).toList
+  Stream(1,2,3).take(4).toList
 
   // drop() tests
-  Stream(1,2,3).drop(2).toList                    //> res5: List[Int] = List(3)
-  Stream(1,2,3).drop(0).toList                    //> res6: List[Int] = List(1, 2, 3)
-  Stream(1,2,3).drop(4).toList                    //> res7: List[Int] = List()
+  Stream(1,2,3).drop(2).toList
+  Stream(1,2,3).drop(0).toList
+  Stream(1,2,3).drop(4).toList
 
   // takeWhile() tests
-  Stream(4,2,3,12).takeWhile( _ % 2 == 0).toList  //> res8: List[Int] = List(4, 2)
-  Stream(1,2,3,4,3,2,1).takeWhile( _ < 4).toList  //> res9: List[Int] = List(1, 2, 3)
+  Stream(4,2,3,12).takeWhile( _ % 2 == 0).toList
+  Stream(1,2,3,4,3,2,1).takeWhile( _ < 4).toList
 
   // takeIf() tests
-  Stream(4,2,3,12).takeIf( _ % 2 == 0).toList     //> res10: List[Int] = List(4, 2, 12)
-  Stream(1,2,3,4,3,2,1).takeIf( _ < 4).toList     //> res11: List[Int] = List(1, 2, 3, 3, 2, 1)
+  Stream(4,2,3,12).takeIf( _ % 2 == 0).toList
+  Stream(1,2,3,4,3,2,1).takeIf( _ < 4).toList
   
   // forAll() tests
-  Stream(1,2,3).forAll( _ < 4)                    //> res12: Boolean = true
-  Stream(1,2,3).forAll( _ < 3)                    //> res13: Boolean = false
-  Stream(1,2,3).forAll( _ > 0)                    //> res14: Boolean = true
-  Stream(1,2,3).forAll( _ > 4)                    //> res15: Boolean = false
+  Stream(1,2,3).forAll( _ < 4)
+  Stream(1,2,3).forAll( _ < 3)
+  Stream(1,2,3).forAll( _ > 0)
+  Stream(1,2,3).forAll( _ > 4)
 
   // exists() tests
-  Stream(1,2,3).exists( _ < 4)                    //> res16: Boolean = true
-  Stream(1,2,3).exists( _ < 3)                    //> res17: Boolean = true
-  Stream(1,2,3).exists( _ > 0)                    //> res18: Boolean = true
-  Stream(1,2,3).exists( _ > 4)                    //> res19: Boolean = false
+  Stream(1,2,3).exists( _ < 4)
+  Stream(1,2,3).exists( _ < 3)
+  Stream(1,2,3).exists( _ > 0)
+  Stream(1,2,3).exists( _ > 4)
   
 }
