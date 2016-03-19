f290aa29a6f0d5f3e03d1d4f06d5c3bb418a4fa8
williamdemeo <williamdemeo@gmail.com>
2016-03-19 17:01:30 -0500

    first few exercises in Ch 8 on testing
    plus finished incomplete exercise 6.8 (using flatMap to implement
    nonNegativeLessThan in State.scala)

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,10 +1,18 @@
-// Authors: Paul Chiusano and Runar Bjarnason
-// Url: https://github.com/fpinscala/fpinscala 
-// 
-// Exercises in this file were solved by williamdemeo@gmail.com.
-// The solutions may be imperfect. Authoritative solutions are provided by 
-// Chiusano and Bjarnason at the github repository cited above.
-
+/* File: Gen.scala (Ch 8)
+ * Authors: Paul Chiusano and Runar Bjarnason
+ * Url: https://github.com/fpinscala/fpinscala 
+ * 
+ * Description: This is a modified version of the file Gen.scala
+ *   that accompanies the book "Functional Programming in Scala" by
+ *   Chiusano and Bjarnason. This version of the file includes 
+ *   solutions to some of the exercises in 
+ * 
+ *     CHAPTER 8: Property-based testing
+ * 
+ *   The solutions herein are by William DeMeo <williamdemeo@gmail.com>.
+ *   They are at best imperfect, and possibly wrong.  Official solutions by 
+ *   Chiusano and Bjarnason are available in the github repo mentioned above.
+ */
 package fpinscala.testing
 
 import fpinscala.laziness.Stream
@@ -25,7 +33,7 @@ trait Prop {
 	//     st is a string that represents the value that caused the failure, and 
 	//     n  is the number of successful cases checked before failure occurred.
 	def check: Either[(FailedCase, SuccessCount), SuccessCount]
-	def &&(p: Prop): Prop = this && p
+	def &&(that: Prop): Prop = this && that
 }
 
 object Prop {
@@ -49,4 +57,49 @@ trait Gen[A] {
 trait SGen[+A] {
 
 }
+
+/* Ex 8.1 To get used to thinking about testing in this way, come up with properties that
+ * specify the implementation of a sum: List[Int] => Int function. You don't have to write 
+ * your properties down as executable ScalaCheck code--an informal description is fine.
+ * Here are some ideas to get you started:
+ *     Reversing a list and summing it should give the same as summing the original list.
+ *     What should the sum be if all elements of the list are the same value?
+ *     Can you think of other properties?
+ *     
+ * Possible Answers: 
+ *     - check commutative and associative laws?
+ *     - summing a list of 0's should equal 0.
+ *     - summing an n-element list where all elements are c should equal n*c.
+ *     - sum(h::tail) should equal h + sum(tail)
+ *     - sum(Nil) = sys.error?
+ *     - sum(l1 append l2) = sum(l1) + sum(l2)
+ *     - sum(l1 append (l2 append l3)) = sum(l1) + sum(l2) + sum(l3) = sum((l1 append l2) append l3)
+ *       (This test wouldn't be necessary if we have already tested append such that the
+ *       two arguments to sum that appear here give the same list.) 
+ */
+
+/* Ex 8.2 What properties specify a function that finds the maximum of a List[Int]?
+ * 
+ * Answer:
+ *     l.forAll(x => (x <= maximum)) // every element of the list is bounded above by maximum
+ *     && l.exists(x => (x == maximum)) // the maximum occurs in the list.
+ * 
+ */
+
+/* Ex 8.3 Assuming the following representation of Prop, implement && as a method of Prop.
+ *         
+ *    trait Prop { def check: Boolean }
+ *     
+ * Answer: 
+ * 
+ * 		trait Prop { 
+ * 			def check: Boolean
+ * 			def &&(that: Prop): Boolean = this && that
+ * 		}
+ */
+
+
+
+
+
 
